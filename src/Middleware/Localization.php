4289d7fcdd4dc9b914c9cca8575afc1cad560432
cf-git <is.captain.fail@gmail.com>
2020-08-05 11:22:52 +0300

    [add] Localization Middleware

---
+++ src/Middleware/Localization.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace CFGit\Middleware;
+
+use Closure;
+use Carbon\Carbon;
+
+class Localization
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param \Illuminate\Http\Request $request
+     * @param \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $locale = $request->route()->parameter('lang');
+        $request->route()->forgetParameter("lang");
+        $locale = in_array($locale, config("app.locales.available")) ? $locale : config('app.locales.default');
+        app('url')->defaults(['lang' => $locale]);
+        app()->setLocale($locale);
+        view()->share([
+            'locale' => $locale,
+            'lang' => $locale
+        ]);
+        setlocale(LC_TIME, $this->longLocaleAlias($locale));
+        \Illuminate\Support\Carbon::setLocale($this->shortLocaleAlias($locale));
+        return $next($request);
+    }
+
+    protected function longLocaleAlias($locale)
+    {
+        $alias = [
+            'ua' => 'uk_UA.UTF-8',
+            'ru' => 'ru_RU.UTF-8',
+        ];
+        return $alias[$locale] ?? $locale;
+    }
+
+    protected function shortLocaleAlias($locale)
+    {
+        $alias = [
+            'ua' => 'uk_UA'
+        ];
+        return $alias[$locale] ?? $locale;
+    }
+}
